6011990adae05bbb6144cec34934f23b4dafc866
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2021-01-28 06:17:16 +0900

    メソッドの定義

--- sample.rb
+++ sample.rb
@@ -1,3 +1,4 @@
+# ファイルに保存したRubyプログラムを実行する
 puts 1 + 2
 
 a = 'Hello,world'
@@ -6,6 +7,7 @@ puts a
 b = 'こんにちは'
 puts b
 
+# if文
 country = 'italy'
 if country == 'japan'
   puts 'こんにちは'
@@ -16,3 +18,42 @@ elsif country == 'italy'
 else
   puts '???'
 end
+
+# メソッドの定義
+def add(a, b)
+ puts a + b
+end
+add(1, 2)
+
+# メソッド名はスネークケースで書く
+def hello_world
+  puts 'Hello, world!'
+end
+
+# キャメルケースは使わない（エラーではないが一般的ではない）
+def helloWorld
+  puts 'Hello, world'
+end
+
+# アンダースコアでメソッド名を描き始める（アンダースコアで始まることは少ない）
+def _hello_world
+  puts 'Hello, world!'
+end
+
+# メソッド名に数字を入れる
+def hello_world_2
+  puts 'Hello, world!!'
+end
+
+# 数字から始まるメソッドは使えない（エラーになる）
+# def 2_hello_world
+#   puts 'Hello, world!!'
+# end
+
+# 変数名をひらがなにする（一般的ではない）
+def あいさつする
+  puts 'はろー、わーるど！'
+end
+
+# ひらがなのメソッドを呼び出す
+あいさつする
