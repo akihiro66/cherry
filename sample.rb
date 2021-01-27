276ce0c3cdd81ee15e349b9812af67210ab3890f
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2021-01-28 05:59:15 +0900

    if文

--- sample.rb
+++ sample.rb
@@ -5,3 +5,14 @@ puts a
 
 b = 'こんにちは'
 puts b
+
+country = 'italy'
+if country == 'japan'
+  puts 'こんにちは'
+elsif country == 'us'
+  puts 'Hello'
+elsif country == 'italy'
+  puts 'ciao'
+else
+  puts '???'
+end
